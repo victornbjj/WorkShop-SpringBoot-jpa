19da09f87ce97aa54b4f56f65abcad7ae8c4fb92
J. Victor Pereira <victorifpb78@gmail.com>
2026-04-12 17:51:02 -0300

    feat: Banco de dados H2, test profile

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,14 +1,26 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
+
     private String email;
+
     private  String phone;
+
     private String password;
 
 
