5c04933b6e6fd8eb8301929af44a71050aa6aebb
J. Victor Pereira <victorifpb78@gmail.com>
2026-04-12 18:08:56 -0300

    feat: implements JPA repository, injeção de dependência, database seeding

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -28,8 +28,7 @@ public class User implements Serializable {
 
     }
 
-    public User(long id, String name, String email, String phone, String password) {
-        this.id = id;
+    public User( String name, String email, String phone, String password) {
         this.name = name;
         this.email = email;
         this.phone = phone;
