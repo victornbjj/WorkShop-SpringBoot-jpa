de2d6afa21795c774a2334f9ea8e15ed346b1e2c
J. Victor Pereira <victorifpb78@gmail.com>
2026-04-12 21:28:18 -0300

    feat:Enum OrderStatus

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course.entities;
 
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus ;
+
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -30,9 +33,10 @@ public class Order implements Serializable {
     }
 
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -59,6 +63,18 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+
+    public OrderStatus getOrderStatus() throws IllegalAccessException {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+
     }
 
     @Override
---
+++ src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
@@ -0,0 +1,31 @@
+package com.educandoweb.course.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERD(4),
+    CANCELED(5);
+
+
+    private int code;
+
+    private OrderStatus (int code){
+        this.code = code;
+    }
+
+    public int getCode(){
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code) throws IllegalAccessException {
+        for (OrderStatus value : OrderStatus.values()) {
+            if(value.getCode() == code){
+                return value;
+            }
+            
+        } throw  new IllegalAccessException("Invalid OrderStatus code");
+
+    }
+
+}
