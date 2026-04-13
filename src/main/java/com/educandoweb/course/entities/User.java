9c887164a3ce234d7e5e37f0ca67e36e0c8e969e
J. Victor Pereira <victorifpb78@gmail.com>
2026-04-12 21:10:28 -0300

    feat:Implementation of the Order Entity. Dates with Instant and ISO standard.

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,8 +1,11 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -22,6 +25,11 @@ public class User implements Serializable {
     private  String phone;
 
     private String password;
+
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
 
     public User(){
@@ -53,6 +61,10 @@ public class User implements Serializable {
 
     public String getEmail() {
         return email;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public void setEmail(String email) {
