852208ff202252190aee496e4797542a0e9bbed2
J. Victor Pereira <victorifpb78@gmail.com>
2026-04-18 21:38:49 -0300

    feat: excption udate

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.dao.DataIntegrityViolationException;
@@ -25,13 +26,21 @@ public class UserService {
         return userRepository.findAll();
     }
 
+
+
     public User findById(Long id){
          Optional <User> obj = userRepository.findById(id);
          return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+
+
+
     public  User insert(User obj){
         return userRepository.save(obj);
     }
+
+
 
     public void  delete(Long id){
         try {
@@ -44,15 +53,22 @@ public class UserService {
         }
 
     }
+
+
+
     public User update(Long id, User obj){
-        if(!userRepository.existsById(id)){
-            throw new ResourceNotFoundException(id);
-        }
-        User entity = userRepository.getReferenceById(id);
-        upateData(entity, obj);
-        return userRepository.save(entity);
+       try {
+           User entity = userRepository.getReferenceById(id);
+           upateData(entity, obj);
+           return userRepository.save(entity);
+       }
+       catch (EntityNotFoundException e){
+              throw new ResourceNotFoundException(id);
+       }
+
         
     }
+
     public void upateData(User entity, User obj){
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
